42ea82d116b940defa8ef4db53bc49d1ce52fbac
Rasyad Bimasatya <rasyadbimasatya04@gmail.com>
2023-01-26 07:39:51 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'production'? 'localhost':'0.0.0.0',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         //If you want a wider scope, alias CORS is enabled for all routes on the server, you can specify CORS in the configuration when you want to create a server by adding the routes.cors property. Examples like this:
         routes: {
             cors: {
